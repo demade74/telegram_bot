06ac80775289d897a7ed217c1f562abc46e521b4
demade74 <demade74@gmail.com>
2019-12-29 12:14:35 +0300

    -

--- db.py
+++ db.py
@@ -1,4 +1,7 @@
 import pymongo
+import os
+from urllib.parse import quote_plus
 
-client = pymongo.MongoClient()
+#mongo_url = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017')
+client = pymongo.MongoClient(os.environ['MONGOLAB_URI'])
 users_collection = client.users_places.users
